c59ada4ab793031f5ce248a75571d53164c722cd
tgriffabenitez <tgriffabenitez@gmail.com>
2023-06-15 01:13:47 -0300

    Documentacion de los metodos de la clase TokenUtils

--- ms-bff/src/main/java/com/sistemasactivos/msbff/utils/TokenUtils.java
+++ ms-bff/src/main/java/com/sistemasactivos/msbff/utils/TokenUtils.java
@@ -33,6 +33,12 @@ public class TokenUtils {
         }
     }
 
+    /**
+     * Parsea un token de acceso y retorna los claims contenidos en él.
+     *
+     * @param token El token de acceso a parsear.
+     * @return Los claims del token de acceso, o null si ocurre un error durante el parsing.
+     */
     public Claims parseToken(String token) {
         try {
             Jws<Claims> claimsJws = Jwts.parserBuilder()
@@ -46,6 +52,12 @@ public class TokenUtils {
         }
     }
 
+    /**
+     * Obtiene los roles contenidos en un token de acceso.
+     *
+     * @param token El token de acceso del cual se obtendrán los roles.
+     * @return Una lista de roles contenidos en el token de acceso, o null si el token no es válido o no contiene roles.
+     */
     public List<String> getRolesFromToken(String token) {
         Claims claims = parseToken(token);
         if (claims != null) {
@@ -54,6 +66,13 @@ public class TokenUtils {
         return null;
     }
 
+    /**
+     * Valida si un token de acceso contiene un rol específico.
+     *
+     * @param token        El token de acceso a validar.
+     * @param requiredRole El rol requerido a verificar en el token.
+     * @return true si el token contiene el rol requerido, false si el token no es válido o no contiene el rol.
+     */
     public boolean validateRoles(String token, String requiredRole) {
         List<String> roles = getRolesFromToken(token);
         return roles != null && roles.contains(requiredRole);
