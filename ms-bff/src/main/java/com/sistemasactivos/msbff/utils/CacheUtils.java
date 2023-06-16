e40e39801245d163e2c8a0e62b4ecb16361ca59c
tgriffabenitez <tgriffabenitez@gmail.com>
2023-06-16 16:11:16 -0300

    Agregue documentacion y modifique el valor de retorno del metodo getValueFromCache, ahora devuelve una lista con todos los roles que tiene el token. Tambien implemente la funcion putDataInCache, la cual almacena el token como key y la lista de roles como el value

--- ms-bff/src/main/java/com/sistemasactivos/msbff/utils/CacheUtils.java
+++ ms-bff/src/main/java/com/sistemasactivos/msbff/utils/CacheUtils.java
@@ -4,8 +4,17 @@ import com.github.benmanes.caffeine.cache.Cache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+/**
+ * Clase utilitaria para el manejo de la caché.
+ */
 @Component
 public class CacheUtils {
+
+    @Autowired
+    private TokenUtils tokenUtils;
+
     private final Cache<String, Object> cache;
 
     @Autowired
@@ -13,11 +22,38 @@ public class CacheUtils {
         this.cache = cache;
     }
 
+    /**
+     * Verifica si un token está presente en la caché.
+     *
+     * @param token el token a verificar.
+     * @return {@code true} si el token está en la caché, {@code false} en caso contrario.
+     */
     public boolean isTokenInCache(String token) {
         return cache.getIfPresent(token) != null;
     }
 
-    public Object getValueFromCache(String token) {
-        return cache.getIfPresent(token);
+    /**
+     * Obtiene el valor asociado a un token en la caché.
+     * Si el token ha expirado, se invalida y se devuelve {@code null}.
+     *
+     * @param token el token del cual obtener el valor en la caché.
+     * @return el valor asociado al token en la caché, o {@code null} si el token ha expirado.
+     */
+    public List<String> getValueFromCache(String token) {
+        if (tokenUtils.isTokenExpired(token)) {
+            cache.invalidate(token);
+            return null;
+        }
+        return (List<String>) cache.getIfPresent(token);
+    }
+
+    /**
+     * Almacena un par clave-valor en la caché.
+     *
+     * @param key   la clave del elemento a almacenar en la caché.
+     * @param value el valor asociado a la clave a almacenar en la caché.
+     */
+    public void putDataInCache(String key, Object value) {
+        cache.put(key, value);
     }
 }
