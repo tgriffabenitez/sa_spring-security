b987e746a766866eaefdc8b979adfb0474894e64
tgriffabenitez <tgriffabenitez@gmail.com>
2023-06-17 00:10:02 -0300

    Devuelvo el rol por el header para poder acceder al ms-usuario

--- ms-bff/src/main/java/com/sistemasactivos/msbff/service/LoginService.java
+++ ms-bff/src/main/java/com/sistemasactivos/msbff/service/LoginService.java
@@ -82,6 +82,7 @@ public class LoginService implements ILoginService {
 
         // Agrego el token al header del response
         response.getHeaders().add("Bearer", "Bearer " + token);
+        response.getHeaders().add("Role", userRol.get(0));
 
         // Almaceno el token y el rol en la cache
         cacheUtils.putDataInCache(token, userRol);
