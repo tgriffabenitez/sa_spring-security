60472fc5305a90c7b354956762052a431b3e183c
tgriffabenitez <tgriffabenitez@gmail.com>
2023-06-17 00:12:19 -0300

    Clase para poder devolver el codigo de error y el mensaje a postman

---
+++ ms-usuario/src/main/java/com/sistemasactivos/msusuario/utils/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.sistemasactivos.msusuario.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private int httpStatusCode;
+    private String message;
+}
